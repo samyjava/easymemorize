0ad49cd3c216601cbd8ec92a942326ecfaa577f8
Yasaman <yasaman_saba@yahoo.com>
2019-11-25 10:38:11 +0330

    Initial Commit

---
+++ EasyMemorize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyMemorize
+//
+//  Created by Yasaman Farahani Saba on 11/25/19.
+//  Copyright © 2019 Dream Catcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
