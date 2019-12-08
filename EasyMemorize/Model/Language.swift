74ae78adad7b8006732aa0689416be380b4070c7
Yasaman <yasaman_saba@yahoo.com>
2019-12-08 16:29:07 +0330

    Create enum Language as a model.

---
+++ EasyMemorize/Model/Language.swift
@@ -0,0 +1,15 @@
+//
+//  Language.swift
+//  EasyMemorize
+//
+//  Created by Yasaman Farahani Saba on 12/8/19.
+//  Copyright © 2019 Dream Catcher. All rights reserved.
+//
+
+import Foundation
+
+enum Language: String {
+    case us = "en-us"
+    case gb = "en-gb"
+    case de = "de-de"
+}
