a32a794775da23790fcb023148a33eb3e3ef4d6d
Yasaman <yasaman_saba@yahoo.com>
2019-12-23 11:51:09 +0330

    Add Scene and Tab files in SceneCoordinator group.

---
+++ EasyMemorize/Business/SceneCoordinator/Scene.swift
@@ -0,0 +1,19 @@
+//
+//  Scene.swift
+//  EasyMemorize
+//
+//  Created by Yasaman Farahani Saba on 12/23/19.
+//  Copyright © 2019 Dream Catcher. All rights reserved.
+//
+
+import Foundation
+
+enum Scene {
+    case splash
+    case register
+    case login
+    case createLesson
+    case createCard
+    case showCards
+    case showBoxCards
+}
---
+++ EasyMemorize/Business/SceneCoordinator/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  EasyMemorize
+//
+//  Created by Yasaman Farahani Saba on 12/23/19.
+//  Copyright © 2019 Dream Catcher. All rights reserved.
+//
+
+import Foundation
+
+
+enum Tab {
+    case dashboard
+    case lesson
+    case leithnerBox
+}
