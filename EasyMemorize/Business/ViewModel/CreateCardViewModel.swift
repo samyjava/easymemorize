84e5e6ba5500c6268ef89f0a854965570bfb277b
Yasaman <yasaman_saba@yahoo.com>
2019-12-28 16:40:40 +0330

    Implement CreateCardViewModel.

--- EasyMemorize/Business/ViewModel/CreateCardViewModel.swift
+++ EasyMemorize/Business/ViewModel/CreateCardViewModel.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import RxSwift
+import Action
+
+struct NewCard {
+    let word: String
+    let definition: String
+    let lesson: LessonItem
+}
 
 struct CreateCardViewModel {
+    let sceneCoordinator: SceneCoordinatorType!
+    let cardService: CardServiceType!
     
+    init(sceneCoordinator: SceneCoordinatorType, cardService: CardServiceType) {
+        self.sceneCoordinator = sceneCoordinator
+        self.cardService = cardService
+    }
+    
+    func save() -> Action<[NewCard],Void> {
+        return Action { cards -> Observable<Void> in
+            Observable.from(cards).flatMap{ card in
+                self.cardService.createCard(word: card.word, definition: card.definition, Lesson: card.lesson)
+            }.do(onError: { error in
+                throw error
+            }, onCompleted: {
+                self.sceneCoordinator.pop()
+            }).asObservable().map{_ in}
+        }
+    }
+    
+    func cancel() -> CocoaAction {
+        return CocoaAction {
+            self.sceneCoordinator.pop().asObservable().map{_ in}
+        }
+    }
 }
